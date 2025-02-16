4a6c9a9f81dbea7a0bc0c60ef2eb059cc95ff859
hdavidpb <68169674+hdavidpb@users.noreply.github.com>
2025-02-16 18:27:37 -0500

    ✖️REMOVE : utils folder removed

--- src/utils/encriptDecript.ts
+++
@@ -1,8 +0,0 @@
-import * as CryptoJS from 'crypto-js';
-
-const KEY = process.env.ENCRIPTED_KEY
-
-export function decrypt(ciphertext:string) {
-    const bytes = CryptoJS.AES.decrypt(ciphertext, KEY!);
-    return bytes.toString(CryptoJS.enc.Utf8);
-}
